a95e9fe170e59569da8736e18ca686a0b70f794b
rodricksnyc <rodricksnyc@gmail.com>
2018-05-24 13:37:50 -0400

    better than before

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function startGame() {
 startGame()
 
 function newGame() {
-    if (guessesRemaining === 10) {
+    if (guessesRemaining > 1) {
 
         let randNum = Math.floor(Math.random() * wordBank.length)
         currentWord = new Word(wordBank[randNum])
@@ -59,38 +59,24 @@ function promptUser() {
         {
             name: 'chosenLetter',
             type: 'input',
-            message: 'Choose a letter',
-            validate: function(value) {
-                if (isLetter(value)) {
-                    return true
-                } else {
-                    return false
-                }
-            }
+            message: 'Choose a letter'
         }
     ]).then(function(ltr) {
 
-        let letterReturned = (ltr.chosenLetter).toUpperCase()
-        let guessedAlready = false
-        console.log(letterReturned)
+        //let letterReturned = (ltr.chosenLetter).toUpperCase()
+        //console.log(letterReturned)
+        //console.log(guessedAlready)
         //   console.log(guessedLetters)
         // for (let i = 0; i < guessedLetters.length; i++) {
             // if(ltr.chosenLetter.toUpperCase() === guessedLetters[i]) {
             //     guessedAlready = true
             // }
-            if (guessedLetters.indexOf(letterReturned) > -1) {
-                guessedAlready = true
-
-            }
-
-
-        // }
-
-        if (guessedAlready === false) {
-
-            guessedLetters.push(letterReturned)
-
-            let found = currentWord.checkIfLetterFound(letterReturned)
+        if (guessedLetters.indexOf(ltr.chosenLetter.toUpperCase()) > -1) {
+            // console.log('You have already guessed that letter')
+            promptUser();
+        } else {
+            guessedLetters.push(ltr.chosenLetter.toUpperCase())
+            let found = currentWord.checkIfLetterFound(ltr.chosenLetter.toUpperCase())
 
             if (found === 0) {
                 console.log('Sorry, wrong guess')
@@ -99,7 +85,8 @@ function promptUser() {
                 console.log('You have ' + guessesRemaining + ' guesses remaining.')
                 console.log(currentWord.animalWord())
                 console.log('Letters guessed: ' + guessedLetters)
-
+                //guessedLetters.push(ltr.chosenLetter.toUpperCase())
+                promptUser();
             } else {
                 console.log('You"re correct')
 
@@ -111,21 +98,60 @@ function promptUser() {
                     console.log('Guesses remaining: ' + guessesRemaining)
                     console.log(currentWord.animalWord())
                     console.log('Letters guessed: ' + guessedLetters)
+                    //guessedLetters.push(ltr.chosenLetter.toUpperCase())
                     promptUser();
                 }
             }
 
-            if (guessesRemaining > 0 && currentWord.wordFound === false) {
-                promptUser();
-            } else if (guessesRemaining === 0) {
+            // if (guessesRemaining > 0 && currentWord.wordFound === false) {
+            //     promptUser();
+            // } else
+            if (guessesRemaining === 0) {
                 console.log('Sorry, the game is over!');
                 console.log('The correct word was: ' + currentWord.word);
                 newGame();
             }
-        } else {
-            console.log('You have already guessed that letter')
-            guessedAlready = false;
-            promptUser();
         }
+
+
+        // }
+
+        // if (!guessedAlready) {
+        //     console.log('in not guessed already')
+        //     guessedLetters.push(letterReturned)
+
+        //     let found = currentWord.checkIfLetterFound(letterReturned)
+
+        //     if (found === 0) {
+        //         console.log('Sorry, wrong guess')
+        //         guessesRemaining--;
+        //         display++
+        //         console.log('You have ' + guessesRemaining + ' guesses remaining.')
+        //         console.log(currentWord.animalWord())
+        //         console.log('Letters guessed: ' + guessedLetters)
+
+        //     } else {
+        //         console.log('You"re correct')
+
+        //         if (currentWord.checkWord() === true) {
+        //             console.log(currentWord.animalWord())
+        //             console.log('You won!');
+        //             startGame();
+        //         } else {
+        //             console.log('Guesses remaining: ' + guessesRemaining)
+        //             console.log(currentWord.animalWord())
+        //             console.log('Letters guessed: ' + guessedLetters)
+        //             promptUser();
+        //         }
+        //     }
+
+        //     if (guessesRemaining > 0 && currentWord.wordFound === false) {
+        //         promptUser();
+        //     } else if (guessesRemaining === 0) {
+        //         console.log('Sorry, the game is over!');
+        //         console.log('The correct word was: ' + currentWord.word);
+        //         newGame();
+        //     }
+        // }
     })
 }
